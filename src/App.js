b9db4a62ea96525acff159726712333445a152ff
Lucimeri Andretta <luandretta@hotmail.com>
2023-06-30 07:55:29 +0000

    Feat: create SingInForm, events and error handlers

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults';
 import SignUpForm from "./pages/auth/SignUpForms";
+import SignInForm from "./pages/auth/SignInForm";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signin" render={() => <SignInForm/>} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
